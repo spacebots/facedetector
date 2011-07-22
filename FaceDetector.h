d917a1a25a7e041762628557fe24ff3c92224028
ferreira <ferreira>
2011-07-22 14:30:26 +0000

    Added Copyright comment

--- FaceDetector.h
+++ FaceDetector.h
@@ -1,3 +1,27 @@
+//$Id: FaceDetector.h,v 1.4 2011/07/22 14:30:26 ferreira Exp $
+
+// Copyright (C) 2008-2011 INESC ID Lisboa.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+
+//$Log: FaceDetector.h,v $
+//Revision 1.4  2011/07/22 14:30:26  ferreira
+//Added Copyright comment
+//
+
 #ifndef FACEDETECT_INCLUDEDEF_H
 #define FACEDETECT_INCLUDEDEF_H
 
